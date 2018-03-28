c24eaf2424584f44587ecd53b8f90756beca418e
yupeiying <pyyu@91jkys.com>
2018-03-28 11:38:54 +0800

    json 数据mock 生成的html页面

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,32 +1,67 @@
 <template>
-  <div>
+<div>
   <div class="mycomponent-wrapper " v-for='(myPageComp,index) in myPageComps' @click='getSelectedIndex(index)'>
     <div :is='myPageComp.name' class=" mycomponent div1" v-model='myPageComp.data'>
       {{myPageComp.name}}
     </div>
   </div>
-  </div>
-
+</div>
 </template>
 <script>
-import { mapState } from 'vuex' // 不用vuex， 用生成的json文件
+// import {
+//   mapState
+// } from 'vuex' // 不用vuex， 用生成的json文件
 import headerBannerImg1 from '../components/header/headerBannerImg1'
 import whiteFooter from '../components/footer/blackFooter'
 import blackFooter from '../components/footer/whiteFooter'
 import paragraph1 from '../components/paragraph/paragraph1'
 import picTitle from '../components/title/picTitle'
 import title1 from '../components/title/title1'
+var myPageComps = {
+  code: '', //活动code
+  pageId: '', //页面埋点
+  title: '测试', //页面标题
+  bodyColor:'#fff',
+  share: {
+    link: '',
+    title: "糖尿病知识百科考试，快来参加！",
+    desc: "来答题，看看你对糖尿病百科了解多少？",
+    content: "来答题，看看你对糖尿病百科了解多少？",
+    imageUrl: ''
+  },
+
+  components: [ //组件
+    {
+      name: 'headerBannerImg1',
+      data: {
+        src: '//static.qa.91jkys.com/index/49bfad6d-cd15-4ce0-8375-6a9511d88575.jpg',
+      }
+    },
+    {
+      name: 'paragraph1',
+      data: {
+        text: '糖尿病是一种代谢紊乱综合征，除血糖高以外，往往还同时伴有血脂代谢异常等，共同构成了糖尿病慢性并发症的高危因素。糖尿病是一种代谢紊乱综合征，除血糖高以外，往往还同时伴有血脂代谢异常等，共同构成了糖尿病慢性并发症的高危因素。',
+      }
+    },
+    {
+      name: 'whiteFooter',
+      data: {
+        backgroundColor: '',
+      }
+    },
+
+  ]
+}
 
 
 export default {
   data() {
     return {
+      myPageComps: myPageComps.components
 
     }
   },
-  computed:mapState([
-    'myPageComps',
-  ]),
+
 
   components: {
     headerBannerImg1,
@@ -40,7 +75,6 @@ export default {
 
 
 }
-
 </script>
 <style scoped lang="scss">
 
